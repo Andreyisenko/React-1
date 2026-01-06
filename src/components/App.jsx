35361f096210f33628b09fd5f9d84882a0d2a768
Andrey Isenko <aisenko87@gmail.com>
2026-01-06 17:15:23 +0200

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
   return (
     <div>
       <Heder />
+      <Heder />
       <Product imgSrc="https://placehold.co/400x400/6495ed/ffffff?text=React" />
       <Product imgSrc="https://placehold.co/400x400/6495ed/ffffff?text=React" />
       <Footer />
