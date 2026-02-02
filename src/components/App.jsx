49291bfe0cb475b4c81ac004379d4c5dd62fed78
Andrey Isenko <aisenko87@gmail.com>
2026-02-02 19:24:22 +0200

    add-localstorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,29 +5,44 @@ import Phone from './Phone/Phone';
 import List from './List/List';
 import User from './User/User';
 import Counter from './Counter/Counter';
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import NewCounter from './NewCounter/NewCounter';
 import TextHiden from './TextHiden/TextHiden';
+import AllClick from './AllClick/AllClick';
+import ClickObj from './ClickObj/ClickObj';
+import Clicks from './Clicks/Clicks';
+import Modal from './Modal/Modal';
+import Modals from './Modal/Modals/Modals';
+import Click2 from './Clicks/Click2/Click2';
 
 const App = () => {
-  const [allClick, setAllClick] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+  const [click, setClick] = useState(0);
+  // useEffect(() => {
+  //   setInterval(() => {
+  //     console.log(`interval ${Date.now()}`);
+  //   }, 2000);
+  // }, []);
+  // const [allClick, setAllClick] = useState(0);
 
-  const changeAllClick = () => setAllClick(prev => prev + 1);
-  const minusAllClick = () => setAllClick(prev => prev - 1);
-  const resAllClick = () => setAllClick(0);
-
+  // const changeAllClick = () => setAllClick(prev => prev + 1);
+  // const minusAllClick = () => setAllClick(prev => prev - 1);
+  // const resAllClick = () => setAllClick(0);
+  // const [allClPlus, setAllClPlus] = useState(0);
+  // const changePlus = () => setAllClPlus(prev => prev + 1);
+  // const changeMinus = () => setAllClPlus(prev => prev - 1);
   // const arr = ["футбол", "баскетбол", "волейбол", "хокей", "теніс", "теніс"];
   // const showHeader = !false;
   return (
     <div>
-      {/* {showHeader ? <Heder /> : <p>Not Heder</p>}
-      <Phone
+      {/* {showHeader ? <Heder /> : <p>Not Heder</p>} */}
+      {/* <Phone
         name="Redmi"
         price={12000}
         imgSrc="https://placehold.co/200x400/6495ed/ffffff?text=Redmi"
-      />
-      <Product imgSrc="https://placehold.co/400x400/6495ed/ffffff?text=React" />
-      <List arrList={arr} /> */}
+      /> */}
+      {/* <Product imgSrc="https://placehold.co/400x400/6495ed/ffffff?text=React" /> */}
+      {/* <List arrList={arr} /> */}
       {/* <User /> */}
       {/* <Counter
         allClick={allClick}
@@ -42,8 +57,29 @@ const App = () => {
         changeAllClick={changeAllClick}
       /> */}
       {/* <NewCounter /> */}
-      <TextHiden user="Taras" />
+      {/* <TextHiden user="Taras" /> */}
       {/* <TextHiden user="Bob" /> */}
+      {/* <AllClick
+        value={allClPlus}
+        onUpdate={changePlus}
+        onUpdateMinus={changeMinus}
+        reset={() => setAllClPlus(0)}
+      />
+      <AllClick
+        onUpdateMinus={changeMinus}
+        value={allClPlus}
+        reset={() => setAllClPlus(0)}
+        onUpdate={changePlus}
+      /> */}
+      {/* <ClickObj /> */}
+      {/* <Clicks /> */}
+      {/* <div>
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? 'Close' : 'Open'}
+        </button>
+        {isOpen && <Modals click={click} />}
+      </div> */}
+      <Click2 />
       <Footer />
     </div>
   );
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,12 +1,19 @@
+import { useEffect } from 'react';
+
 const Modal = ({ price, onClose }) => {
+  useEffect(() => {
+    setInterval(() => {
+      console.log(`interval ${Date.now()}`);
+    }, 2000);
+  }, []);
   return (
     <div
       style={{
-        width: "100vw",
-        height: "100vh",
-        backgroundColor: "rgba(0, 0, 0, 0.5)",
+        width: '100vw',
+        height: '100vh',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
         // backgroundColor: "green",
-        position: "fixed",
+        position: 'fixed',
         top: 0,
         left: 0,
         zIndex: 10,
@@ -14,23 +21,23 @@ const Modal = ({ price, onClose }) => {
     >
       <div
         style={{
-          width: "50%",
-          height: "50%",
-          backgroundColor: "rgba(26, 182, 78, 0.8)",
+          width: '50%',
+          height: '50%',
+          backgroundColor: 'rgba(26, 182, 78, 0.8)',
           // backgroundColor: "green",
-          position: "fixed",
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)",
+          position: 'fixed',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
           zIndex: 20,
-          textAlign: "center",
-          padding: "20px",
-          border: "1px solid",
-          boxShadow: "0 0 60px rgba(188, 255, 2)",
-          borderRadius: "20px",
+          textAlign: 'center',
+          padding: '20px',
+          border: '1px solid',
+          boxShadow: '0 0 60px rgba(188, 255, 2)',
+          borderRadius: '20px',
         }}
       >
-        <p style={{ color: "yellow" }}>Ціна телефону: {price} грн</p>
+        <p style={{ color: 'yellow' }}>Ціна телефону: {price} грн</p>
         <button onClick={() => onClose()}>Close</button>
       </div>
     </div>
